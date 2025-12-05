8f0dd850c9b135208c5512fb0d338a171da0cca2
Aldlanga80 <aldo.d.langa@gmail.com>
2025-12-05 15:35:13 -0300

    Agregado de authRoutes

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,1 +1,11 @@
 import { Router } from "express";
+import { register, login } from "../controllers/authController"
+import { registerValidator, loginValidator } from "../utils/validators"
+import { validationResultHandler } from "../middlewares/validationHandler"
+
+const router = Router();
+
+router.post("/register", registerValidator, validationResultHandler, register);
+router.post("/login", loginValidator, validationResultHandler, login);
+
+export default router;
